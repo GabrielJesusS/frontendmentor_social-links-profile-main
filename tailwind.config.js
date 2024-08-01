c1700ff5f8daca945309a59b03cb3593387589f5
Gabriel Jesus <gabrieljesus9221@gmail.com>
2024-08-01 20:58:54 -0300

    feat: adds settings to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./source/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["inter", "serif"],
+      },
+      colors: {
+        "alt-green": "rgba(197 248 42 / <alpha-value>)",
+        "alt-grey": {
+          700: "rgba(51 51 51 / <alpha-value>)",
+          800: "rgba(31 31 31 / <alpha-value>)",
+          900: "rgba(20 20 20 / <alpha-value>)",
+        },
+      },
+    },
   },
   plugins: [],
 };
